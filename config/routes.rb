862fa31266fd70c245b99398aed5afbcabb61001
Rahul Agarwal <Rahul@Admins-MacBook-Pro.local>
2021-01-15 01:43:22 +0530

    added signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'  -- or
+  resources :users, except: [:new]
   resources :articles#, only: [:show, :index, :new, :create, :edit , :update, :destroy]
 end
