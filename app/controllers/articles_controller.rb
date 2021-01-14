ef0183e57699b89ef1f521463f144ae7de45b89a
Rahul Agarwal <Rahul@Admins-MacBook-Pro.local>
2021-01-15 00:08:18 +0530

    Added one to many associations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,8 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article]
     @article = Article.new(article_param)
+    @article.user_id = User.first.id
+    byebug
     if @article.save
       flash[:notice] = "Articles is created successfully"
       redirect_to article_path(@article)  #this will extract id 
